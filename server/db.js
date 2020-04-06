d84c515c5254e79c9b587cedaf90d4e7c5368100
Alexander Tarlyovski <alexander.tarlyovski@questio.solutions>
2020-04-06 13:14:54 +0300

    Bump deps and switch to async DB file access

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileAsync');
+const FileAsync = require('lowdb/adapters/FileAsync');
 
-const adapter = new FileSync('db.json');
+const adapter = new FileAsync('db.json');
 const db = low(adapter);
 
 const API_KEY = "{YOUR_OPENWEATHERMAP_API_KEY}";
